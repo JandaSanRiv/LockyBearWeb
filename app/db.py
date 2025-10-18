6e62babdd66394e03a7f5264f8cb8775918446a9
Alma Sanchez <sanchezalmr@globalhitss.com>
2025-10-18 12:05:31 -0700

    adding fastapi to the project

--- app/db.py
+++ app/db.py
@@ -14,12 +14,17 @@ def init_db(conn):
         print(f"Database Sqlite3 not created successfully, e: {e}")
 def add_entity(conn, entity):
     try:
+        entity_id, = get_entity_id(conn,entity)
+        if entity_id>0:
+            return {"response": 200, "id": entity_id, "name": entity}
         cursor = conn.cursor()
         cursor.execute('''INSERT INTO entities(name, created_at) VALUES ("'''+entity+'''", CURRENT_DATE)''')
-        conn.commit()
+        #conn.commit()
         print(f"Record inserted for: {entity}")
+        return {"response": 201, "id": cursor.lastrowid, "name": entity }
     except Exception as e:
-        print(f"Error inserting entity: {entity}. Error: {e}")
+        print(f"Error inserting entity: {entity}. Error: {e}!")
+        return {"response": 400, "id": -1, "name": entity }
 def get_entity_id(conn, entity_name):
     cursor = conn.cursor()
     cursor.execute('''SELECT id from entities WHERE name = "'''+entity_name+'''"''')
@@ -28,26 +33,61 @@ def get_entity_id(conn, entity_name):
     if row is not None:
         return row
     else:
-        return -1
-    
+        return -1,#because it's a tuple
 def add_password(conn, entity, password):
     entity_id, = get_entity_id(conn, entity)
     print(entity_id)
     if entity_id <0:
-        return "Error: The entity does nto exist!"
+        print("Error: The entity does not exist!")
+        return {"response": 404, "entity": entity, "password": password}
 
+    if get_password_id(conn, entity)[0]>0:
+        print("Warning: The password already exists!")
+        return {"response": 404, "entity": entity, "password": password}
     try:
+        print("adding password!")
         cursor = conn.cursor()
         cursor.execute(
-            '''INSERT INTO passwords(entity_id, secret, created_at) VALUES ("''' + password + '''", CURRENT_DATE)''')
+            '''INSERT INTO passwords(entity_id, secret, created_at) VALUES ("''' + str(entity_id) + '''","''' + password + '''", CURRENT_DATE)''')
         conn.commit()
-        return f"password {cursor.lastrowid} for entity {entity} added successfully"
-    except:
-        return "Error adding the password!"
-def get_passwords():
-    conn = sqlite3.connect("passwords.db")
+        print(f"Password {cursor.lastrowid} for entity {entity} added successfully")
+        return {"response": 201, "id": cursor.lastrowid, "name": entity }
+    except Exception as e:
+        print(f"Error inserting password: {password}, entity: {entity}. Error: {e}")
+        return {"response": 400, "id": -1, "name": password, "entity":entity}
+def get_password_id(conn, entity_id):
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM passwords")
+    cursor.execute('''SELECT id from passwords WHERE entity_id = "'''+entity_id+'''"''')
+    row = cursor.fetchone()
+    print("Getting entity id")
+    if row is not None:
+        return row
+    else:
+        return -1,#because it's a tuple
+def get_entities(conn):
+    entities = []
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * from entities''')
     rows = cursor.fetchall()
-    return rows
+    print("Getting entities")
+    if rows:
+        print("There are entities")
+        for row in rows:
+            print(row)
+            entities.append({"id": row[0], "name": row[1]})
+    return entities
+
+
+def get_passwords(conn):
+    passwords = []
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * from passwords''')
+    rows = cursor.fetchall()
+    print("Getting passwords")
+    if rows:
+        print("There are passwords")
+        for row in rows:
+            print(row)
+            passwords.append({"entity_id": row[0], "secret": row[1], "created_at": row[2]})
+    return passwords
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 import sqlite3
-from xml.dom.minidom import Entity
 
 from fastapi import FastAPI
 import db
-conn = sqlite3.connect("passwords.db")
+conn = sqlite3.connect("passwords5.db", timeout=10)
 
 app = FastAPI()
 @app.on_event("startup")
@@ -11,30 +10,44 @@ def on_startup():
     #This runs once then the app starts
     db.init_db(conn)
     addRecord()
+    conn.commit()
     print("Database initialized")
-    print(db.get_passwords())
-
-
 
 def addRecord():
-    db.add_entity(conn,"frog")
-    #db.add_entity(conn,"duck")
-    #db.add_password(conn, "horse", "newPWD")
-    #db.add_password(conn, "duck", "oldPWD2")
-"""@app.get("/")
+    db.add_entity(conn,"UNICORN")
+    db.add_entity(conn,"duck")
+    db.add_entity(conn,"Dog")
+
+    db.add_password(conn, "UNICORN", "newPWD")
+    db.add_password(conn, "duck", "oldPWD2")
+    db.add_password(conn, "Dog", "oldPWD3")
+
+    db.add_entity(conn,"GoogleAccount")
+    db.add_password(conn, "GoogleAccount", "YouRNice2")
+    list_entities = db.get_entities(conn)
+    print("entities returned are: ")
+    print(list_entities)
+
+    list_passwords = db.get_passwords(conn)
+    print("passwords returned are: ")
+    print(list_passwords)
+@app.get("/")
 async def root():
-    return {"message": "Hello World"}"""
+    return {"message": "Hello World"}
 """
+@app.get("/api/entities")
+async def entities():
+    return db.get_entities(conn)
+
 @app.post("/api/entities")
 async def entities(body: {}):
-    return body
-@app.get("/api/entities")
-async def entities(entities_list: {}):
-    return entities_list
+    response = db.add_entity(conn,body["entity"])
+    return response
 @app.post("/api/passwords")
-async def passwords(entity: str, password: str):
-    return password
+async def passwords(body: {}):
+    response = db.add_password(conn,body["entity"],body["password"])
+    return response
 
 @app.get("/api/passwords/{entity}")
-async def entities(entity: str, password: str):
-    return {entity, password}"""
+async def passwords():
+    return db.get_passwords(conn)"""
